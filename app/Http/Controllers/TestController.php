42c24e2f40ba1cd001c83e574bd40825fd34eab7
wangguan <422721922@qq.com>
2018-09-14 10:19:59 +0800

    Initial commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+/**
+ * Created by PhpStorm.
+ * User: wangguan
+ * Date: 2018/9/13
+ * Time: 下午5:15
+ */
+class TestController extends Controller
+{
+
+    public function __construct()
+    {
+
+    }
+
+
+    public function index(){
+
+        echo 1;die;
+    }
+
+
+}
